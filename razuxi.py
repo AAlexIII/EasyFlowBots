3ff351bb2ecf4d81d04467415079500ad02bb4b8
AAlexIII <a.alex.2000@mail.ru>
2020-04-09 23:42:06 +0300

    heroku1

--- razuxi.py
+++ razuxi.py
@@ -1,12 +1,19 @@
 import telebot
-from telebot.types import KeyboardButton, ReplyKeyboardMarkup,InlineKeyboardMarkup,InlineKeyboardButton
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 import csv
 import pandas as pd
 
+bot = telebot.TeleBot('1181885362:AAHGMG3zg3KWfPWOuX1xSVgbzreiSAZ5WSs')
+
 
 @bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.id, 'Напишите вашу фамилию')
+
 
 @bot.message_handler(content_types=['text'])
+def send_mes(message):
+    bot.send_message(message.id, 'куку')
 
 
 bot.polling(none_stop=True)
